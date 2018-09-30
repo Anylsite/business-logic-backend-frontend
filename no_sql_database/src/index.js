88fd4e0d84078702640cbc2d4114b598333bcd58
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 18:24:56 +0530

    register sensorData api handler

--- no_sql_database/src/index.js
+++ no_sql_database/src/index.js
@@ -1,9 +1,11 @@
 const ping = require('./api/ping');
 const notFound = require('./api/notFound');
 const sensors = require('./api/sensors');
+const sensorData = require('./api/sensorData');
 
 module.exports = {
   ping,
   notFound,
   sensors,
+  sensorData,
 };
