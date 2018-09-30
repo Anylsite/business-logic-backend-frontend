f779354daf1af81c70d5aad2950447547b946193
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 22:28:17 +0530

    spin up another component for App

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,24 +1,12 @@
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route } from 'react-router-dom';
+import App from './App';
 
-import { history, store, theme } from './config';
 import './index.css';
-import { Home, Login } from './pages';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <MuiThemeProvider theme={theme}>
-        <Route exact path="/" component={Home.Page} />
-        <Route exact path="/login" component={Login.Page} />
-      </MuiThemeProvider>
-    </ConnectedRouter>
-  </Provider>,
+  <App />,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );
