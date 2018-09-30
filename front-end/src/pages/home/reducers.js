b4def713d3d28c651c36168f9a4326137ac467d3
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 02:39:17 +0530

    save the response from the api server to the redux state

--- front-end/src/pages/home/reducers.js
+++ front-end/src/pages/home/reducers.js
@@ -1,9 +1,21 @@
+import { Types } from './actions';
+
 const initialState = {
-  foo: 'bar',
+  sensors: [],
+  isProcessing: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case Types.FETCH_SENSORS_PROCESSING:
+      return Object.assign({}, state, { isProcessing: true });
+    case Types.FETCH_SENSORS_SUCCESS:
+      return Object.assign({}, state, {
+        sensors: action.payload,
+        isProcessing: false,
+      });
+    case Types.FETCH_SENSORS_FAIL:
+      return Object.assign({}, state, { isProcessing: false });
     default:
       return state;
   }
