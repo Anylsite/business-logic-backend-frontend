ae302a4d6d68e997e11904287144368f532fcc45
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 17:22:30 +0530

    send status and message along with the response

--- no_sql_database/src/api/notFound.js
+++ no_sql_database/src/api/notFound.js
@@ -1,3 +1,13 @@
 const { send } = require('micro');
 
-module.exports = (req, res) => send(res, 404, 'Not found route');
+/**
+ * Raised a not found error when the route does not exists
+ *
+ * @param req
+ * @param res
+ * @returns {*}
+ */
+module.exports = (req, res) => send(res, 404, {
+  status: 'fail',
+  message: 'The api endpoint you are looking for does not exists',
+});
