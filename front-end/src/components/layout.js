68eed5428ccb391d945d68a5a4ec9c35f6bbd8a6
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 02:38:09 +0530

    Layout now has Grid System

--- front-end/src/components/layout.js
+++ front-end/src/components/layout.js
@@ -1,9 +1,21 @@
+import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import AppHeader from './AppHeader';
 
 const Layout = Component => props => ([
   <AppHeader key="appBarHeader" />,
-  <Component key="PageBodyContent" {...props} />,
+  <Grid container key="PageBodyContent" direction="row" spacing={16}>
+    <Grid item direction="column" lg={8} md={8} sm={12} spacing={32}>
+      <div className="p-20 content-container">
+        <Component {...props} />
+      </div>
+    </Grid>
+    <Grid item direction="column" lg={4} md={4} sm={12}>
+      <div className="p-20 content-container">
+        Some Content Here...
+      </div>
+    </Grid>
+  </Grid>,
 ]);
 
 export default Layout;
