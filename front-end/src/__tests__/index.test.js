889b33a0a6e5723a47ab65bdf535a543e4790ee5
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 23:10:00 +0530

    moved tests file to its own folder

--- front-end/src/__tests__/index.test.js
+++ front-end/src/__tests__/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
