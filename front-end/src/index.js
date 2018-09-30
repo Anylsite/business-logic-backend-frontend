3f4ae6da16f2b7efdcbaf1d16b8ee85638c09a00
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 22:13:36 +0530

    setup the root component along with the router configuration

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,25 @@
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+
+import { history, store, theme } from './config';
 import './index.css';
-import App from './App';
+import { Home, Login } from './pages';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <MuiThemeProvider theme={theme}>
+        <Route exact path="/" component={Home.Page} />
+        <Route exact path="/login" component={Login.Page} />
+      </MuiThemeProvider>
+    </ConnectedRouter>
+  </Provider>,
+  // eslint-disable-next-line no-undef
+  document.getElementById('root'),
+);
 registerServiceWorker();
