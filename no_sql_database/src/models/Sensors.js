fc7a671c32ff0dceac59ebaa1083adc6472a7c84
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 17:49:01 +0530

    the model now extends the adapter
    this would help us to achieve modularity on choosing the right adapter
    for the model

--- no_sql_database/src/models/Sensors.js
+++ no_sql_database/src/models/Sensors.js
@@ -1,3 +1,15 @@
-const FileModel = require('./adapters/FileBaseClass');
+/**
+ * Sensors Model
+ *
+ * A model that is responsible to get sensors data from the backend.
+ * This model currently is connected to the FileSystem, however, it could extends the Blockchain
+ * base model and read data from Blockchain
+ *
+ * @type {FileModel}
+ */
+const FileModel = require('./adapters/FileSystem');
 
-module.exports = new FileModel();
+class Sensors extends FileModel {
+}
+
+module.exports = Sensors;
