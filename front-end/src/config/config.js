ee51498058dd77ef5bd0e36570a43cfcd434e1e7
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-09-30 23:13:21 +0530

    configuration file for the web app

---
+++ front-end/src/config/config.js
@@ -0,0 +1,11 @@
+export default {
+  app: {
+    name: 'AnyLedger Web',
+    colors: {
+      light: '#1abc9c',
+      main: '#16a085',
+      dark: '#27ae60',
+      contrastText: '#fff',
+    },
+  },
+};
