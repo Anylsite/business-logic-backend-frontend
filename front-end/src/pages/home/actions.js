601c3237161dc96e839a767a471006e11e6ab7cb
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 16:03:38 +0530

    action creators and actions added for fetching single sensor data

--- front-end/src/pages/home/actions.js
+++ front-end/src/pages/home/actions.js
@@ -5,11 +5,15 @@ export const Types = {
   FETCH_SENSORS_PROCESSING: 'FETCH_SENSORS_PROCESSING',
   FETCH_SENSORS_SUCCESS: 'FETCH_SENSORS_SUCCESS',
   FETCH_SENSORS_FAIL: 'FETCH_SENSORS_FAIL',
+  FETCH_SENSORS_DATA: 'FETCH_SENSORS_DATA',
+  FETCH_SENSORS_DATA_PROCESSING: 'FETCH_SENSORS_DATA_PROCESSING',
+  FETCH_SENSORS_DATA_SUCCESS: 'FETCH_SENSORS_DATA_SUCCESS',
+  FETCH_SENSORS_DATA_FAIL: 'FETCH_SENSORS_DATA_FAIL',
 };
 
 export const fetchSensors = () => (dispatch) => {
   dispatch({ type: Types.FETCH_SENSORS_PROCESSING });
-  const apiUrl = `${Config.api.url}/sensors`;
+  const apiUrl = `${Config.api.url}/sensors.json`;
   try {
     // eslint-disable-next-line no-undef
     fetch(apiUrl)
@@ -18,14 +22,44 @@ export const fetchSensors = () => (dispatch) => {
         if (data.status === 'success') {
           dispatch({ type: Types.FETCH_SENSORS_SUCCESS, payload: data.data });
         } else {
-          dispatch({ type: Types.FETCH_SENSORS_FAIL });
+          dispatch({ type: Types.FETCH_SENSORS_FAIL, payload: data.message });
         }
       }).catch((err) => {
-        // eslint-disable-next-line no-console
+      // eslint-disable-next-line no-console
         console.error(err.message, err);
         dispatch({ type: Types.FETCH_SENSORS_FAIL, payload: err });
       });
   } catch (err) {
     dispatch({ type: Types.FETCH_SENSORS_FAIL, payload: err });
+  }
+};
+
+export const fetchSensorData = sensorId => (dispatch) => {
+  dispatch({ type: Types.FETCH_SENSORS_DATA_PROCESSING, payload: sensorId });
+  const apiUrl = `${Config.api.url}/sensors/${sensorId}/data.json`;
+  try {
+    // eslint-disable-next-line no-undef
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.status === 'success') {
+          dispatch({
+            type: Types.FETCH_SENSORS_DATA_SUCCESS,
+            payload: {
+              sensorId,
+              data,
+            },
+          });
+        } else {
+          dispatch({
+            type: Types.FETCH_SENSORS_DATA_FAIL,
+            payload: { sensorId, err: { message: data.message } },
+          });
+        }
+      });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err.message, err);
+    dispatch({ type: Types.FETCH_SENSORS_DATA_PROCESSING, payload: { sensorId, err } });
   }
 };
