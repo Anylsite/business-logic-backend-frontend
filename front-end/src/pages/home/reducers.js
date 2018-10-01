895ae461ff67558a790e3e9fee68aa323f9ccc08
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 16:03:16 +0530

    updated reducers to make sure it works with enzyme

--- front-end/src/pages/home/reducers.js
+++ front-end/src/pages/home/reducers.js
@@ -2,20 +2,56 @@ import { Types } from './actions';
 
 const initialState = {
   sensors: [],
+  sensorData: {},
   isProcessing: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case Types.FETCH_SENSORS_PROCESSING:
+    case Types ? Types.FETCH_SENSORS_PROCESSING : 'FETCH_SENSORS_PROCESSING':
       return Object.assign({}, state, { isProcessing: true });
-    case Types.FETCH_SENSORS_SUCCESS:
+
+    case Types ? Types.FETCH_SENSORS_DATA_PROCESSING : 'FETCH_SENSORS_DATA_PROCESSING':
+      return Object.assign({}, state, {
+        sensorData: {
+          [action.payload.sensorId]: {
+            processing: true,
+            ...state.sensorData[action.payload.sensorId],
+          },
+          ...state.sensorData,
+        },
+      });
+
+    case Types ? Types.FETCH_SENSORS_SUCCESS : 'FETCH_SENSORS_SUCCESS':
       return Object.assign({}, state, {
         sensors: action.payload,
         isProcessing: false,
       });
-    case Types.FETCH_SENSORS_FAIL:
+
+    case Types ? Types.FETCH_SENSORS_DATA_SUCCESS : 'FETCH_SENSORS_DATA_SUCCESS':
+      return Object.assign({}, state, {
+        sensorData: {
+          [action.payload.sensorId]: {
+            processing: false,
+            data: action.payload.data,
+          },
+          ...state.sensorData,
+        },
+      });
+
+    case Types ? Types.FETCH_SENSORS_FAIL : 'FETCH_SENSORS_FAIL':
       return Object.assign({}, state, { isProcessing: false });
+
+    case Types ? Types.FETCH_SENSORS_DATA_FAIL : 'FETCH_SENSORS_DATA_FAIL':
+      return Object.assign({}, state, {
+        sensorData: {
+          [action.payload.sensorId]: {
+            processing: false,
+            ...state.sensorData[action.payload.sensorId],
+          },
+          ...state.sensorData,
+        },
+      });
     default:
       return state;
   }
