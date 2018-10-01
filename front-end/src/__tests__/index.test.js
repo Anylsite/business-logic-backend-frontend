b76890a6b7eacabe832f4c79f7f980273d4b0f64
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 14:06:25 +0530

    updated test for testing the entire app at once

--- front-end/src/__tests__/index.test.js
+++ front-end/src/__tests__/index.test.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-undef */
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
