996e31a7498ad930c5dd6c4c165398bba9a275e5
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-01 16:37:14 +0530

    Moved the grid layout system to the actual content for better UI

--- front-end/src/components/layout.js
+++ front-end/src/components/layout.js
@@ -5,16 +5,9 @@ import AppHeader from './AppHeader';
 const Layout = Component => props => ([
   <AppHeader key="appBarHeader" />,
   <Grid container key="PageBodyContent" direction="row">
-    <Grid item lg={8} md={8} sm={12}>
-      <div className="p-20 content-container">
-        <Component {...props} />
-      </div>
-    </Grid>
-    <Grid item lg={4} md={4} sm={12}>
-      <div className="p-20 content-container">
-        Some Content Here...
-      </div>
-    </Grid>
+    <div className="p-20 content-container">
+      <Component {...props} />
+    </div>
   </Grid>,
 ]);
 
